07d9f9b8e6768c00a955730cb9991b763cfda83d
Stephen Kirby <me@skirby.dev>
2021-07-12 15:49:13 -0500

    removed NeurIPS work.

--- nips21/lib/Annotators.py
+++
@@ -1,8 +0,0 @@
-from lib.annotators.Annotator import Annotator
-from lib.annotators.Class_Map_Annotator import Class_Map_Annotator
-from lib.annotators.Dynamic_Annotator import Dynamic_Annotator
-from lib.annotators.Dynamic_Preloading_Annotator import Dynamic_Preloading_Annotator
-from lib.annotators.Segment_Annotator import Segment_Annotator
-"""
-Easy access for annotator classes. 
-"""
