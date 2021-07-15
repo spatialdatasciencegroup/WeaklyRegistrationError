94c9a8c81c3d212200428f239c33e4be598cc2f6
Stephen Kirby <me@skirby.dev>
2021-07-15 02:45:38 -0500

    Added splash for setup.py

--- setup.py
+++ setup.py
@@ -4,23 +4,29 @@ Setup Script
 Prepares environment for preprocessing, model training, and EM tests.
 
 ToDo:
-    Initialize folders.
-    Prompt path replacements for config.py
-    Figure out how to force dependencies
-
+    pip and conda requirements
+    Add source data download link
 """
-print("Starting setup script.")
 
 import os
+
+from numpy.testing._private.utils import print_assert_equal
 if not os.path.exists('./config.py'):
     from shutil import copyfile
     copyfile('./config_template.py', './config.py')
+    print("Created config.py in root.")
     #raise FileNotFoundError("Tests require configuration file in root project directory under the name 'config.py'")
 
 # Print environment configuration
 from config import *
 import pathlib
 required_dirs = [INPUT_DATA_DIR, TENSOR_DIR, RESULTS_DIR]
-for env_dir in required_dirs:
+dir_infos = ["Raw Input Data", "Processed Tensor Output", "Results output"]
+for di, env_dir in zip(dir_infos, required_dirs):
     if not os.path.exists(env_dir):
         pathlib.Path(env_dir).mkdir(parents=True)
+        print(f"Created folder for '{di}' at '{env_dir}'")
+
+print("Setup Complete.")
+print("To run the Geometric Annotation Errors EM Pipeline, download the required data from [LINK TBD].\n")
+print("See more info in README.md")
